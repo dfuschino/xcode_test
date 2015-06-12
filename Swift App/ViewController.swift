0a5695bed8d8c25126321022ca530883de2c3062
Derreck.Fuschino <derreckfuschino@admins-MacBook-Pro-2.local>
2015-06-11 20:17:37 -0400

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Derreck.Fuschino on 6/11/15.
+//  Copyright (c) 2015 Derreck.Fuschino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
