a32724a8e9daaeac937aa034e0797481dc67a8ab
Derreck.Fuschino <derreckfuschino@admins-MacBook-Pro-2.local>
2015-06-13 14:15:20 -0400

    added and placed some labels

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -11,10 +11,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //declre ivars
+    var myFirstLabel:UILabel!
+    var iPhoneLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("Hello World")
+        //create labels
+        myFirstLabel = UILabel()
+        myFirstLabel.text = "My First"
+        myFirstLabel.font = UIFont.systemFontOfSize(36)
+        myFirstLabel.sizeToFit()
+        myFirstLabel.center = CGPoint(x: 200, y: 60)
+        view.addSubview(myFirstLabel)
+        
+        iPhoneLabel = UILabel()
+        iPhoneLabel.font = UIFont.boldSystemFontOfSize(48)
+        iPhoneLabel.text = "iPhone app"
+        iPhoneLabel.sizeToFit()
+        iPhoneLabel.center = CGPoint(x: 200, y: 200)
+        view.addSubview(iPhoneLabel)
     }
 
     override func didReceiveMemoryWarning() {
